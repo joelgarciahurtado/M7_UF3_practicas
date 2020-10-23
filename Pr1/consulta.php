c751dcbd3f745450dd3ec5948a8005acb18102f6
joelgarciahurtado <joelgarciah@inslapineda.cat>
2020-10-23 10:52:56 +0200

    git de la primera practica

---
+++ Pr1/consulta.php
@@ -0,0 +1,39 @@
+<html>
+ <head>
+ 	<title>Consulta cuitats</title>
+ 	<style>
+ 		body{
+ 		}
+ 		table,td {
+ 			border: 1px solid black;
+ 			border-spacing: 0px;
+ 		}
+ 	</style>
+ </head>
+ 
+ <body>
+ 	<h1>Exemple de lectura de dades a MySQL</h1>
+ 
+ 	<?php
+ 		# (1.1) Connectem a MySQL (host,usuari,contrassenya)
+ 		$conn = mysqli_connect('172.17.0.1','root','root');
+ 
+ 		# (1.2) Triem la base de dades amb la que treballarem
+ 		mysqli_select_db($conn, 'world');
+ 
+ 		# (2.1) creem el string de la consulta (query)
+ 		$consulta = "SELECT * FROM id="dades"";
+
+ 		echo $_POST["dades"];
+ 
+ 		# (2.2) enviem la query al SGBD per obtenir el resultat
+ 		$resultat = mysqli_query($conn, $consulta);
+ 
+ 		# (2.3) si no hi ha resultat (0 files o bé hi ha algun error a la sintaxi)
+ 		#     posem un missatge d'error i acabem (die) l'execució de la pàgina web
+ 		if (!$resultat) {
+     			$message  = 'Consulta invàlida: ' . mysqli_error() . "\n";
+     			$message .= 'Consulta realitzada: ' . $consulta;
+     			die($message);
+ 		}
+ 	?>
